69b59adc004463d3d3d9151c824a81bffcc7669d
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2022-10-20 21:14:02 +0300

    Add files via upload

---
+++ Functions Advanced/Lab/multiplication_function.py
@@ -0,0 +1,10 @@
+def multiply(*args):
+    res = 1
+    for num in args:
+        res *= int(num)
+    return res
+
+
+print(multiply(1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))
