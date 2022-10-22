63ef79c96bba497ac2fdd38bc699e26433a04e1c
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2022-10-22 17:32:31 +0300

    Add files via upload

---
+++ Exams/14 April 2022/ramen_shop.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+bowls_of_ramen = [int(x) for x in input().split(", ")]
+customers = deque([int(x) for x in input().split(", ")])
+
+while bowls_of_ramen and customers:
+
+    ramen = bowls_of_ramen.pop()
+    customer = customers.popleft()
+
+    if ramen == customer:
+        continue
+
+    if ramen > customer:
+        ramen -= customer
+        bowls_of_ramen.append(ramen)
+        continue
+
+    customer -= ramen
+    customers.appendleft(customer)
+
+if not customers:
+    print("Great job! You served all the customers.")
+
+    if bowls_of_ramen:
+        print(f"Bowls of ramen left: {', '.join([str(x) for x in bowls_of_ramen])}")
+else:
+    print("Out of ramen! You didn't manage to serve all customers.")
+
+    if customers:
+        print(f"Customers left: {', '.join([str(x) for x in customers])}")
